b21e49475eabb86fc7088bae5bd7857cdd4288c1
kocompsci <kocompsci@KOCOMPSCI7.k-o.org>
2026-06-26 11:22:06 -0400

    Convert to CommonJS for Vercel compatibility

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // Only allow POST requests
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -68,4 +67,4 @@ export default async function handler(req, res) {
       message: error.message
     });
   }
-}
+};
